7f09ec06b65ddcf888441878131d675db83e3f5b
vadim-sid1993 <79473969+vadim-sid1993@users.noreply.github.com>
2022-11-07 12:05:50 +0300

    task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const oldFolder = path.join(__dirname, 'files');
+const newFolder = path.join(__dirname, 'files-copy');
+
+(async function copyDir() {
+    try {
+
+      await fs.rm(newFolder, { recursive: true, force: true });
+      await fs.mkdir(newFolder, { recursive: true });
+      const assets = await fs.readdir(oldFolder);
+
+      for (let el of assets) {
+        const old = path.join(oldFolder, el);
+        const novel = path.join(newFolder, el);
+        await fs.copyFile(old, novel);
+      }
+      
+      console.log('Скопировано');
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  })();
 
